ff16074502a384b58d93cad83196350113a79f5c
yashpl <yashpaliwal14@gmail.com>
2019-05-20 19:12:09 +0530

    removed robot class

--- src/webapp/pages/set_location.java
+++ src/webapp/pages/set_location.java
@@ -5,7 +5,9 @@ import java.awt.Robot;
 import java.awt.event.KeyEvent;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class set_location {
 WebDriver driver;
@@ -19,25 +21,21 @@ WebDriver driver;
 		
 	}
 	public void loc_logo() throws InterruptedException {
-		Thread.sleep(2000);
+		Thread.sleep(1000);
 		driver.findElement(loc_logo).click();
 	}
 	
 	public void slctr_loc() throws InterruptedException {
-		Thread.sleep(4000);
+		Thread.sleep(1000);
 		driver.findElement(slctr_loc).click();
 	}
 	
 	public void locality() throws InterruptedException, AWTException {
-		Thread.sleep(2000);
-		driver.findElement(locality).sendKeys("rajajinagar");
-		Robot r=new Robot();
-		r.keyPress(KeyEvent.VK_KP_DOWN);
-		Thread.sleep(500);
-		r.keyRelease(KeyEvent.VK_KP_DOWN);
-		r.keyPress(KeyEvent.VK_ENTER);
-		Thread.sleep(500);
-		r.keyRelease(KeyEvent.VK_ENTER);
+		Thread.sleep(1000);
+		WebElement loc = driver.findElement(locality);
+        loc.sendKeys("Rajajinagar");
+        loc.sendKeys(Keys.DOWN);
+        loc.sendKeys(Keys.RETURN);
 		
 	
 	
