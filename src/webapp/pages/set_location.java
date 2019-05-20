3b6c4c185c197625263018b65b4eec4d22f034ff
yashpl <yashpaliwal14@gmail.com>
2019-05-20 16:12:04 +0530

    Humble Beginnings

---
+++ src/webapp/pages/set_location.java
@@ -0,0 +1,49 @@
+package webapp.pages;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class set_location {
+WebDriver driver;
+	
+	By loc_logo=By.id("header_loc_btn");
+	By slctr_loc=By.xpath("//*[@id=\"dialogContent_12\"]/div/div[2]/md-grid-list/md-grid-tile[1]/figure/md-card/md-card-content/div[2]/div");
+	By locality=By.xpath("//*[@id=\"dialogContent_12\"]/div/div[4]/input");
+	
+	public set_location(WebDriver driver) {
+		this.driver=driver;
+		
+	}
+	public void loc_logo() throws InterruptedException {
+		Thread.sleep(2000);
+		driver.findElement(loc_logo).click();
+	}
+	
+	public void slctr_loc() throws InterruptedException {
+		Thread.sleep(4000);
+		driver.findElement(slctr_loc).click();
+	}
+	
+	public void locality() throws InterruptedException, AWTException {
+		Thread.sleep(2000);
+		driver.findElement(locality).sendKeys("rajajinagar");
+		Robot r=new Robot();
+		r.keyPress(KeyEvent.VK_KP_DOWN);
+		Thread.sleep(2000);
+		r.keyRelease(KeyEvent.VK_KP_DOWN);
+		r.keyPress(KeyEvent.VK_ENTER);
+		Thread.sleep(2000);
+		r.keyRelease(KeyEvent.VK_ENTER);
+		
+	
+	
+	
+
+}
+
+
+}
---
+++ src/webapp/testcases/location.java
@@ -0,0 +1,30 @@
+package webapp.testcases;
+
+import java.awt.AWTException;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import webapp.pages.*;
+
+public class location {
+
+		@Test
+		public void verifyvalidlocation() throws InterruptedException, AWTException {
+			System.setProperty("webdriver.chrome.driver","/home/osboxes/Downloads/chromedriver");
+			WebDriver driver=new ChromeDriver();
+			driver.manage().window().maximize();
+			driver.get("http://v2.20160301.testing.bro4u.com/v2/bro4u-webapp-v2/");
+			driver.manage().timeouts().implicitlyWait(5000,TimeUnit.MICROSECONDS);
+			set_location loc=new set_location(driver);
+			loc.loc_logo();
+			loc.slctr_loc();
+			loc.locality();
+			
+		}
+		
+		
+
+	}
